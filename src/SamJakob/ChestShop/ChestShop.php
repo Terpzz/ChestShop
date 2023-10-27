f7e73c9097bc3196df7fbec4d02bb6536c9fed50
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-27 00:51:46 -0700

    Update ChestShop.php

--- src/SamJakob/ChestShop/ChestShop.php
+++ src/SamJakob/ChestShop/ChestShop.php
@@ -12,12 +12,12 @@ class ChestShop extends PluginBase {
     
     const MESSAGE_TIMEOUT = 60;
     
-    public function onEnable(){
+    public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents(new SignListener($this), $this);
         $this->getLogger()->info("Plugin was successfully enabled.");
     }
     
-    public function onDisable(){
+    public function onDisable(): void {
         $this->getLogger()->info("Plugin was successfully disabled.");
     }
     
